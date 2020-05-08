7c59b8cb441fb20c436be1e8022c92d52c70ed89
Fer Torres <fertorresmx@gmail.com>
2020-05-08 13:25:41 -0500

    Using static files. Add images and logo. Applying styles

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -3,12 +3,13 @@ const path = require("path");
 const app = express();
 const productsRouter = require('./routes/products');
 
+app.use("/static", express.static(path.join(__dirname, "public")));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use("/products", productsRouter);
 
-
 const server = app.listen(8000, function() {
-	console.log(`Listening http://localhost:${server.address().port}`);
+  console.log(`Listening http://localhost:${server.address().port}`);
 });
