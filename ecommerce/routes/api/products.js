4ad19c5e6c0628168ffe8cf9289461f29d6d157a
Fer Torres <fertorresmx@gmail.com>
2020-05-08 18:46:54 -0500

    Finish CRUD methods. Using mocks and services. Installing body-parser

--- ecommerce/routes/api/products.js
+++ ecommerce/routes/api/products.js
@@ -1,44 +1,78 @@
 const express = require("express");
 const router = express.Router();
-const productMocks = require('../../utils/mocks/products');
+const ProductsService = require('../../services/products');
 
-router.get('/', function(req, res){
-  const { query } = req;
+const ProductService = new ProductsService();
 
-  res.status(200).json({
-    data: productMocks,
-    message: 'products listed'
-  });
+router.get('/', async function(req, res, next){
+  const { tags } = req.query;
+  try {
+    const products = await ProductService.getProducts({ tags });
+
+    res.status(200).json({
+      data: products,
+      message: 'products listed'
+    });
+  } catch(err) {
+    next(err)
+  }
 });
 
-router.get('/:productId', function(req, res){
+router.get('/:productId', async function(req, res){
   const { productId } = req.params;
+  try {
+    const product = await ProductService.getProduct({ productId });
 
-  res.status(200).json({
-    data: productMocks[0],
-    message: 'product retrieved'
-  });
+    res.status(200).json({
+      data: product,
+      message: 'product retrieved'
+    });
+  } catch(err) {
+    next(err)
+  }
 });
 
 router.post('/', function(req, res){
-  res.status(201).json({
-    data: productMocks,
-    message: 'products listed'
-  });
+  const { body: product } = req;
+  try {
+    const createdProduct = ProductService.createProduct({ product });
+
+    res.status(201).json({
+      data: createdProduct,
+      message: 'products created'
+    });
+  } catch(err) {
+    next(err)
+  }
 });
 
 router.put('/:productId', function(req, res){
-  res.status(200).json({
-    data: productMocks[0],
-    message: 'product updated'
-  });
+  const { productId } = req.params;
+  try {
+    const { body: product } = req;
+    const updatedProduct = ProductService.updateProduct({ productId, product });
+
+    res.status(200).json({
+      data: updatedProduct,
+      message: 'product updated'
+    });
+  } catch(err) {
+    next(err)
+  }
 });
 
 router.delete('/:productId', function(req, res){
-  res.status(200).json({
-    data: productMocks[0],
-    message: 'product deleted'
-  });
+  const { productId } = req.params;
+  try {
+    const deletedProduct = ProductService.deleteProduct({ productId });
+
+    res.status(200).json({
+      data: deletedProduct,
+      message: 'product deleted'
+    });
+  } catch(err) {
+    next(err)
+  }
 });
 
 module.exports = router;
--- ecommerce/routes/products.js
+++ ecommerce/routes/products.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const productMocks = require('../utils/mocks/products');
+const ProductsService = require('../services/products');
 
-router.get("/", function(req, res) {
-  res.render("products", { products });
+const ProductService = new ProductsService();
+
+router.get("/", async function(req, res) {
+  const { tags } = req.query;
+
+  try {
+    const products = await ProductService.getProducts({ tags });
+    res.render("products", { products });
+  }
+  catch(err) {
+    next(err);
+  }
 });
 
 module.exports = router;
