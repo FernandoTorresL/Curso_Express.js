254cf58791fea8fd28d0698cccfc82061a1aa133
Fer Torres <fertorresmx@gmail.com>
2020-05-09 00:56:11 -0500

    Using Sentry

--- ecommerce/routes/views/products.js
+++ ecommerce/routes/views/products.js
@@ -8,6 +8,7 @@ router.get("/", async function(req, res, next) {
   const { tags } = req.query;
 
   try {
+    myUndefinedFunction();
     throw new Error('This is an error');
     const products = await ProductService.getProducts({ tags });
     res.render("products", { products });
